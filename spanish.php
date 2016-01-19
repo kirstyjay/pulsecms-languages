37b8fa065ff4362af20dc350dc05793e247a43e6
kirstyjay <kirstyjay@gmail.com>
2016-01-19 10:50:25 +0000

    Update spanish.php

--- spanish.php
+++ spanish.php
@@ -7,17 +7,17 @@ $lang_save = "Guardar";
 $lang_rename_btn = "Renombrar";
 $lang_title = "Pulse CMS";
 $lang_version = "Pulse CMS 4.5.2";
-$lang_update = "Update";
+$lang_update = "Actualizar";
 
 // Login
 
 $lang_login_incorrect = "Contraseña Incorrecta!";
 $lang_login_password = "Contraseña";
-$lang_login_button = "Acceder";
+$lang_login_button = "Iniciar sesión";
 $lang_nav_logout = "Salir";
 
 // Home
-$lang_home_emptyfold = "Carpeta Vacía."; 
+$lang_home_emptyfold = "La carpeta está vacía."; 
 $lang_home_new = "Nuevo";
 $lang_home_upload_button = "Subir";
 $lang_home_preview = "Previsualizar";
@@ -32,18 +32,18 @@ $lang_nav_pages = "Páginas";
 
 // Gallery
 
-$lang_gal_caption_gallery = "Insertar texto aquí";
+$lang_gal_caption_gallery = "Inserta el texto aquí";
 $lang_gal_filename = "Archivo";
 $lang_gal_dimensions = "Dimensiones";
 $lang_gal_size = "Tamaño";
 $lang_gal_img = "Imagen";
 $lang_gal_link = "Enlace";
-$lang_gallery_tool_tip = "To create a lightbox gallery, create a folder of images and use the embed tag.";
+$lang_gallery_tool_tip = "Para crear una galería de 'lightbox', Hay que crear una carpeta de imágenes y usa la etiqueta 'embed'.";
 
 // Pages
 
-$lang_pages_title = "Titulo de Página";
-$lang_pages_description = "Descripción de la Página";
+$lang_pages_title = "Titulo de la página";
+$lang_pages_description = "Descripción de la página";
 
 // Create
 
@@ -53,23 +53,23 @@ $lang_create_tool_tip = "Para crear nuevas páginas, bloques y entradas del Blog
 
 // Delete
 
-$lang_delete = 'Seguro que quieres borrar ';
+$lang_delete = '¿Está seguro que quiere borrar? ';
 $lang_del_button = "Borrar";
 
 // Errors
 
-$lang_error_create_ext = 'Solo puedes crear archivos de texto y carpetas.';
+$lang_error_create_ext = 'Solo puede crear archivos de texto y carpetas.';
 $lang_error_file_exists = 'Ya existe un archivo con este nombre en la carpeta.';
-$lang_error_upload = 'Necesitas estar en una Galería para subir una imagen.';
-$lang_no_content = 'sin contenido';
+$lang_error_upload = 'Necesita estar en una galería para subir una imagen.';
+$lang_no_content = 'sin contenidos';
 
 //Blog
 
 $lang_blog_read_more = 'Leer más';
 $lang_blog_older = 'Anteriores';
 $lang_blog_newer = 'Nuevos';
-$lang_blog_error_folder = "No puedes crear una carpeta en Blog";
-$lang_blog_tool_tip = "Blog entries are ordered by number, the highest numbering being the newest post.";
+$lang_blog_error_folder = "No puede crear una carpeta en Blog";
+$lang_blog_tool_tip = "Las entradas del blog están ordenado por número, el número más grande va primero.";
 
 
 //Form
@@ -78,20 +78,20 @@ $lang_form_error1 = 'Completa el campo ';
 $lang_form_error2a = 'Tu campo ';
 $lang_form_error2b = ' es demasiado largo.';
 $lang_form_subject_line = 'Contacto';
-$lang_form_email_sent = 'Correo enviado';
+$lang_form_email_sent = 'Su correo ya está enviado.';
 $lang_form_sent_button = 'Enviar';
 
 // Stats
 
-$lang_stats_today = "Visitors";
-$lang_stats_refers = "Top Referrers";
-$lang_stats_pages = "Top Pages";
-$lang_stats_refresh = "Refresh";
-$lang_stats_pageviews = "Pageviews";
-$lang_stats_per_visit = "Pages / Visit";
+$lang_stats_today = "Visitantes";
+$lang_stats_refers = "Mejores referentes";
+$lang_stats_pages = "Mejores páginas";
+$lang_stats_refresh = "Actualizar";
+$lang_stats_pageviews = "Vistas de la página";
+$lang_stats_per_visit = "Páginas / Vistas";
 $lang_stats_online = "Online";
-$lang_stats_thisweek = "This Week";
-$lang_stats_todays_stats = "Today";
-$lang_stats_nodata = "No Data";
+$lang_stats_thisweek = "Está semana";
+$lang_stats_todays_stats = "Hoy";
+$lang_stats_nodata = "Sin datos";
 
 ?>
